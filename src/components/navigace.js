b0ae80475bf9073690af042915bdabb255c873ce
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2022-11-08 17:21:02 +0100

    vytvořena a nastylována komponenta navigace.js

---
+++ src/components/navigace.js
@@ -0,0 +1,15 @@
+import "./Navigace.css"
+
+function Navigace() {
+    return(
+        <div className="navigace">
+            <ul>
+                <li><a href="#">Kdo jsem</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Životopis</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navigace
